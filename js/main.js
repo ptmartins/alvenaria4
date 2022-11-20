50aebfac80a4b494b5bd3f456bb30d02b1c981f0
Pedro Martins <snitramordep@gmail.com>
2022-11-20 18:01:47 +0000

    More style tweaks

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-const header = document.querySelector('.header');
+(function() {
+    let header = document.querySelector('.header');
 
-window.addEventListener('scroll', () => {
-    const scrollPos = window.scrollY;
-    if(scrollPos > 10){
-        header.classList.add ('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-})
+    window.addEventListener('scroll', () => {
+        let scrollPos = window.scrollY;
+        if(scrollPos > 10){
+            header.classList.add ('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    })
 
-$(document).ready(function(){
+    $(document).ready(function(){
 
-    console.log('slick');
-
-    $('.slide-show').slick({
-        autoplay: true,
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
+        $('.slide-show').slick({
+            autoplay: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear'
+        });
     });
-  });
+    
+})();
