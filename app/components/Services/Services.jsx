7986039b7915f817be905c4c64dd9fecbfa8a57b
ptmartins <pedro@pedromartins.com.pt>
2023-11-19 15:51:51 +0000

    Add react icons to services tiles.

--- app/components/Services/Services.jsx
+++ app/components/Services/Services.jsx
@@ -1,6 +1,9 @@
 import { createClient } from 'contentful';
 import contentfulConfig from '../../../contentfulConfig';
 import styles from './Services.module.css'; 
+import { FaPaintRoller, FaBath, FaTools, FaHammer, FaRecycle } from "react-icons/fa";
+import { BsHousesFill } from "react-icons/bs";
+import { MdKitchen } from "react-icons/md";
 
 async function getServicesData() {
     const client = createClient(contentfulConfig),
@@ -14,7 +17,29 @@ async function getServicesData() {
 export default async function Services() {
 
     const data = await getServicesData(),
-          services = data.items;
+          services = data.items,
+          getServiceIcon = (service) => {
+            switch(service) {
+                case 'Construção de moradias':
+                    return <FaHammer className={styles.icon} />;
+                    break;
+                case 'Pintura de fachadas':
+                    return <FaPaintRoller className={styles.icon} />;
+                    break;
+                case 'Remodelações':
+                    return <BsHousesFill className={styles.icon} />;
+                    break;
+                case 'Remodelações de cozinhas':
+                    return <MdKitchen className={styles.icon} />;
+                    break;
+                case 'Remodelações de casas de banho':
+                    return <FaBath className={styles.icon} />;
+                    break;
+                case 'Reabilitação de edificios':
+                    return <FaRecycle className={styles.icon} />;
+                    break;                             
+            }
+          };
 
     return(
         <section className="section section--services">
@@ -24,6 +49,7 @@ export default async function Services() {
                     {services.map((service, index) => {
                         return(
                             <div className={styles.service}>
+                                {getServiceIcon(service.fields.servico)}
                                 <h3 key={index}>{service.fields.servico}</h3>
                             </div>
                         )
