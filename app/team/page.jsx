eeb93fec735a5b4092a2e3e02e0de81a228c664f
ptmartins <pedro@pedromartins.com.pt>
2023-11-19 06:28:09 +0000

    Connect to Contentful API

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -1,14 +1,28 @@
 import Image from 'next/image';
 import styles from './team.module.css';
 
-export default function Team() {
+import { createClient } from 'contentful';
+import contentfulConfig from '../../contentfulConfig';
+
+async function getTeamData() {
+    const client = createClient(contentfulConfig),
+          res = await client.getEntries();
+
+    return res; 
+}
+
+
+export default async function Team() {
+
+    const data = await getTeamData();
+
     return(
         <div className="page">
             <h1 className="page__title">A equipa</h1>
             <div className={styles.team}>
                 <div className={styles.team_member}>
                     <div className={styles.thumbnail}>
-                        <Image src="/images/team/filipe_campelo.jpeg" fill="true" className={styles.thumbnail_image} />
+                        <Image src="/images/team/filipe_campelo.jpeg" alt="foo" fill="true" className={styles.thumbnail_image} />
                     </div>
                     <div className={styles.body}>
                         <span className={styles.role}>Director Técnico</span>
